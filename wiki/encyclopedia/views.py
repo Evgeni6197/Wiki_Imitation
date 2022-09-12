be5f9757872f1a3fefbed7842f2fddefd4069fb5
Evgeni6197 <kevg6197@gmail.com>
2022-09-13 02:28:42 +0300

    12th september coding result

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -24,4 +24,40 @@ def entry(request, entry_name):
         return render(request, "encyclopedia/entry.html", {
             'entry_name':'Not Found',
             'not_found':'404 <br> Page Not Found'
-        })        
+        })    
+
+def search(request):
+    if request.method == 'POST':
+        query = request.POST['q']
+        entries = util.list_entries()
+        # if the user's query matches one of the existing wiki entries
+        if query in entries:
+            return render(request, "encyclopedia/entry.html", {
+                'entry_name':query,
+                'entry_html_content':markdown2.markdown(util.get_entry(query))
+                })
+        
+        else:
+            results=[]  #list of wiki entries for which query is a subsrting
+
+            for entry in entries:
+                print(entry, '  ', query) 
+                if query in entry:
+                    results.append(entry)
+            print(results)      
+            if results:
+                return render(request, "encyclopedia/search.html",{
+                    'results':results
+                    }) 
+            else:
+                return  render(request, "encyclopedia/entry.html", {
+                'entry_name':'Not Found',
+                'not_found':'404 <br> Page Not Found'
+                })  
+
+
+          
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "search1":"input your query"
+        })
